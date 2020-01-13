e981e0550a034869db1bfcf7535bc11f7de99a9e
Christopher Ware <warech@bc.edu>
2020-01-13 18:35:23 -0500

    Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Chris Ware on 1/13/20.
+//  Copyright © 2020 Chris Ware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func sayItPressed(_ sender: UIButton) {
+        messageLabel.text = "Swiftacular"
+    }
+}
+
